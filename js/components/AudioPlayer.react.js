9949c01d4e3570a300586105f5e691dadf187b02
defnotryan <ryan@theredremedy.com>
2016-02-25 22:48:11 -0500

    Fix mobile audio
    The root issue was that in iOS, audio elements cannot autoplay, and the
    play method must be called during a user interaction event.

--- js/components/AudioPlayer.react.js
+++ js/components/AudioPlayer.react.js
@@ -7,26 +7,53 @@ import { connect } from 'react-redux';
 import { asyncPlayAudio, asyncPauseAudio } from '../actions/AppActions';
 
 class AudioPlayer extends Component {
-  render() {
-    const { dispatch, src, isAudioPlaying } = this.props;
 
-    if (isAudioPlaying) {
-      return (
-        <span className="audioContainer">
-          <button className="pauseAudio" onClick={() => {dispatch(asyncPauseAudio())} }>
-            <i className="fa fa-pause" title="pause"></i>
-          </button>
-          <audio src={ src } autoPlay="autoPlay" loop="loop"/>
-        </span>
-      );
-    } else {
-      return (
-        <span className="audioContainer">
-          <button className="playAudio" onClick={() => {dispatch(asyncPlayAudio())} }>
-            <i className="fa fa-play" title="play"></i>
-          </button>
-        </span>
-      );
+  renderPlayButton() {
+    const { dispatch } = this.props;
+    this._audioElement = null;
+    return (
+      <button className="playAudio" onClick={() => {dispatch(asyncPlayAudio());}}>
+        <i className="fa fa-play" title="play"></i>
+      </button>
+    );
+  }
+
+  renderPauseButtonAndAudio() {
+    const { dispatch } = this.props;
+    return (
+      <span>
+        <button className="pauseAudio" onClick={() => {dispatch(asyncPauseAudio())}}>
+          <i className="fa fa-pause" title="pause"></i>
+        </button>
+        { this.renderAudioElement() }
+      </span>
+    );
+  }
+
+  renderAudioElement() {
+    const { src } = this.props;
+    return (
+      <audio ref={(me) => {this._audioElement = me}} loop="loop">
+        <source src={ src } type="audio/mp3"/>
+      </audio>
+    );
+  }
+
+  render() {
+    const { isAudioPlaying } = this.props;
+    return (
+      <span className="audioContainer">
+        { isAudioPlaying ? this.renderPauseButtonAndAudio() : this.renderPlayButton() }
+      </span>
+    )
+  }
+
+  componentDidUpdate() {
+    if (this.props.isAudioPlaying) {
+      // Call load in case we change verses while the audio is playing
+      // Or else the audio element will keep using the old src
+      this._audioElement.load();
+      this._audioElement.play();
     }
   }
 }
